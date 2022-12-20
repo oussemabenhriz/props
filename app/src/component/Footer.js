60d5fc04a29e815799e87abc880d28c50207bb0a
oussemabenhriz <oussemabenhariz33@gmail.com>
2022-12-20 21:50:16 +0100

    first commit

---
+++ app/src/component/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer =() =>{
+    return(
+        <footer>
+        <p>Copyritghts &copy; GOMYCODE 2021</p>
+      </footer>
+    )
+}
+export default Footer;
